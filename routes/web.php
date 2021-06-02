9f7916f2fd36f543fc575bef43c50927f8e804bf
GBlok02 <61229884+GBlok02@users.noreply.github.com>
2021-06-02 17:16:09 +0200

    Update web.php
    routes aangemaakt voor welcome blade en contact blade

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'WelcomeController@index');
+Route::get('/contact', 'WelcomeController@contact');
