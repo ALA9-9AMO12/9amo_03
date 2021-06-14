592af13be06c345ba5f8d15dbf522f4a85141ef1
Saif1903 <60920860+Saif1903@users.noreply.github.com>
2021-06-14 11:34:19 +0200

    Home pagina

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,36 +1,67 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
+
+          integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Imker vereniging</title>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" />
 </head>
 <body>
 
-<div class="container">
-    @yield('main')
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Features</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Pricing</a>
-                    </li>
-                </ul>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"
+
+        integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0"
+
+        crossorigin="anonymous"></script>
+@yield('main')
+<section class="navigation">
+    <div class="container">
+
+        <nav class=" navbar navbar-expand-lg bg-warning">
+            <div class="container-fluid position-relative">
+
+
+                <div class="collapse navbar-collapse " id="navbarNav">
+                    <ul class="nav navbar-nav mx-auto">
+                        <li class="nav-item text-dark">
+                            <a class="nav-link text-dark active" aria-current="page" href="#">HOME</a>
+                        </li>
+                        <li class="nav-item text-dark">
+                            <a class="nav-link text-dark" href="#">BASISCURSUS</a>
+                        </li>
+                        <li class="nav-item text-dark">
+                            <a class="nav-link text-dark" href="#">INFORMATIE</a>
+                        </li>
+                        <li class="nav-item text-dark">
+                            <a class="nav-link text-dark" href="#">LINKS</a>
+                        </li>
+                        <li class="nav-item text-dark">
+                            <a class="nav-link text-dark" href="#">CONTACT</a>
+                        </li>
+                        <li class="nav-item text-dark">
+                            <a class="nav-link text-dark" href="#">BERICHTEN</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
+        </nav>
+    </div>
+</section>
+
+<section class="breaking">
+    <div class="container">
+        <div class="col-9">
+
         </div>
-    </nav>
-</div>
+        <div class="col-3">
+
+        </div>
+    </div>
+</section>
+
+
 
 <script src="{{ asset('js/app.js') }}" type="text/js"></script>
 </body>
