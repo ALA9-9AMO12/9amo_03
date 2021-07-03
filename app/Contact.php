56580d6bd1ca96d5a725341e2985b1a0852b9080
GBlok02 <61229884+GBlok02@users.noreply.github.com>
2021-07-03 14:45:01 +0200

    Update Contact.php

--- app/Contact.php
+++ app/Contact.php
@@ -9,9 +9,6 @@ class Contact extends Model
     protected $fillable = [
         'first_name',
         'last_name',
-        'email',
-        'city',
-        'country',
-        'job_title'
+        'email'
     ];
 }
